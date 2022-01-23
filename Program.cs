a42fe8c4a7d50b9e49bb8f2dbb6c2366495ae8b1
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-01-23 17:41:49 -0500

    made transactions class and began menu (while loop)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+    // create Transaction class to support both checking and savings as well as deposits and withdrawls.
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        //store a history of transactions in a SINGLE List<Transaction>.
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
     }
-  }
-}
+
+    class Program
+    {
+
+
+        // The application should load past transactions from a file when it first starts.
+        static void Main(string[] args)
+        {
+            var database = new TransactionDatabase();
+
+            database.LoadTransactions();
+
+            var keepGoing = true;
+
+            while (keepGoing)
+            {
+                Console.WriteLine("");
+
+                Console.WriteLine("Please choose an option: ");
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("Make a (d)eposit ");
+                //create deposit method that prompts for account: savings or checking
+
+                Console.WriteLine("Make a (w)ithdrawl ");
+                //create withdrawl method that prompts for account: savings or checking
+                Console.WriteLine("(R)emove a dinosaur from the park entirely ");
+
+                Console.WriteLine("(T)ransfer a dinosaur to a different enclosure ");
+                Console.WriteLine("(S)ummarize dinosaur diet types ");
+                Console.WriteLine("(Q)uit ");
+                Console.WriteLine("-------------------------------------------");
+
+                var choice = Console.ReadLine().ToLower();
+
+                switch (choice)
+                {
+                    case "d":
+                        break;
+
+                    case "w":
+                        break;
+
+                    case "Q":
+                        keepGoing = false;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("That is not a valid response. Please try again.");
+                        break;
+
+
+                }
+                database.SaveTransactions();
+
+
+                // As a user I should have a menu option to see the balance of my savings and checking.
+                // As a user I should be able to see the list of transactions designated savings.
+                // As a user I should be able to see the list of transactions designated checking.
+
+                // The application should, after each transaction, write all the transactions to a file. This is the same file the application loads.
+            }
+        }
+    }
+
+
+
+
+
+
+// Never allow withdrawing more money than is available. That is, we cannot allow our checking or savings balances to go negative.
+// When prompting for an amount to deposit or withdraw always ensure the amount is positive...
+//...The value we store in the Transaction shall be positive as well. (e.g. a Transaction that is a withdraw of 25 both inputs and records a positive 25)
+
